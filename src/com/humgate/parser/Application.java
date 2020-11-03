5e0efb139c452832a99285097f87cadfb15113e6
Pavel Yanovsky <humgate@gmail.com>
2020-11-04 00:05:07 +0300

    logging.properties file tuned and added to resources, main Application class added, logger initialization based on logging.properties added

---
+++ src/com/humgate/parser/Application.java
@@ -0,0 +1,38 @@
+package com.humgate.parser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.*;
+
+/**
+ * Main application class. Loads logging configuration from logging.properties
+ */
+public class Application {
+
+    static {
+        //Read logging.properties
+        InputStream stream = Application.class.getClassLoader().getResourceAsStream("logging1.properties");
+        if (stream == null) {
+            System.out.println("Critical error: logging properties file not found. Exiting now");
+            System.exit(2);
+        }
+
+        //read logging config if the resource found
+        try {
+            LogManager.getLogManager().readConfiguration(stream);
+        } catch (IOException e) {
+            System.out.println("Failed to read logging properties from file. Will now exit");
+            System.exit(2);
+        }
+    }
+
+    // Init global package wide logger
+    private static final Logger LOGGER= Logger.getLogger("com.humgate.parser");
+
+    public static void main(String[] args) {
+        LOGGER.info("main method starts");
+        LOGGER.info("main method ends");
+    }
+}
+
+
