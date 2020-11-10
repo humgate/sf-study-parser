8fca056c1059230e91ea94a6af442433159c5c91
Pavel Yanovsky <humgate@gmail.com>
2020-11-11 02:46:19 +0300

    Added jsoup lib to the project. Implemented very simple test of html page fetch

--- src/com/humgate/parser/Application.java
+++ src/com/humgate/parser/Application.java
@@ -1,5 +1,11 @@
 package com.humgate.parser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.*;
@@ -31,6 +37,35 @@ public class Application {
 
     public static void main(String[] args) {
         LOGGER.info("main method starts");
+        Validate.isTrue(args.length == 1, "usage: supply url to fetch");
+        String url = args[0];
+        System.out.println("Fetching %s..." + url);
+
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+            LOGGER.info("got html page into jsoup documents");
+        } catch (IOException e) {
+            System.out.println("Failed to get the webpage from URL");
+        }
+
+
+        Elements links = doc.select("a[href]");
+        Elements media = doc.select("[src]");
+        Elements imports = doc.select("link[href]");
+
+
+        System.out.print("\nMedia: (" + media.size () + ")");
+        for (Element src : media) {
+            if (src.normalName().equals("img"))
+                System.out.println(" * %s: <%s> %sx%s (%s)" +  src.tagName() +
+                                src.attr("abs:src") +  src.attr("width") +
+                                src.attr("height") + (src.attr("alt")));
+            else
+            System.out.println(" * %s: <%s>" + src.tagName() + src.attr("abs:src"));
+        }
+
+
         LOGGER.info("main method ends");
     }
 }
