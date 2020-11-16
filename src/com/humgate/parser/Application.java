638e26398c5ddc3af62fc9da3656c0fee94942e1
Pavel Yanovsky <humgate@gmail.com>
2020-11-17 01:20:22 +0300

    Implemented writing vacation collection to file as Application class method

--- src/com/humgate/parser/Application.java
+++ src/com/humgate/parser/Application.java
@@ -1,19 +1,26 @@
 package com.humgate.parser;
 
-import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.logging.*;
 
 /**
  * Main application class. Loads logging configuration from logging.properties
+ * Creates specific parser. Loads the first search results page where finds how much total search results pages exist.
+ * Saves the links to each specific search result page in a collection. Then for each link to search result page loads
+ * the page and adds all topics information into another collection. Finally iterates throughout topic links collection
+ * loads each vacation details page and adds vacation full description from loaded vacation page inyo the vacations
+ * collection. Finally saves each element og vacations collection to file
  */
+
 public class Application {
     static {
         //Read logging.properties
@@ -30,9 +37,34 @@ public class Application {
             System.exit(2);
         }
     }
-
     // Init global package-wide logger
     private static final Logger LOGGER = Logger.getLogger("com.humgate.parser");
+
+
+    // Write vacations collection to file
+    private static void writeVacationsToFile(LinkedList<Vacation> vacations, String fileName, String searchString) {
+        Date date = new Date();
+        DateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+
+        try (BufferedWriter bufFileWriter = new BufferedWriter(new FileWriter(fileName, false))) {
+            bufFileWriter.write("Vacations search results. Received " + format.format(date)+ ".\n" );
+            bufFileWriter.write("Search string: " + searchString + "\n\n" );
+            int i = 1;
+            for (Vacation vac : vacations) {
+                bufFileWriter.write(i + ". ");
+                bufFileWriter.write("Text = " + vac.getVacTopicText() + "\n");
+                bufFileWriter.write("URL = " + vac.getVacPageURL() + "\n");
+                bufFileWriter.write("Description  = " + vac.getVacDescription() + "\n\n");
+                i++;
+                }
+            bufFileWriter.flush();
+            LOGGER.info("Successfully written vacations to file - " + fileName);
+        } catch (IOException e) {
+            LOGGER.severe(e.getMessage());
+            LOGGER.severe("Failed to write file " + fileName + " Exiting now.");
+            System.exit(2);
+        }
+    }
 
     public static void main(String[] args) {
         LOGGER.info("main method starts");
@@ -49,11 +81,8 @@ public class Application {
         //Iterate through vacationList, read specific vacation page and read description to Vacation in VacationList
         vacationsList = sqlRuParser.getVacationDescription(vacationsList);
 
-        for (Vacation vac : vacationsList) {
-             System.out.println("URL = " + vac.getVacPageURL());
-             System.out.println("Text = " + vac.getVacTopicText());
-             System.out.println("Description  = " + vac.getVacDescription());
-        }
+        //Write vacations info to file
+        writeVacationsToFile(vacationsList, "results.txt", url);
     }
 }
 
